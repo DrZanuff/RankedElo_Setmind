a94df848bde2c803438716183cfd3d5e5e1cff25
Ricardo <ricardo.machado.nwi@gmail.com>
2020-05-22 01:35:35 -0300

    Bear Added

--- Scenes/Base/BaseScene.gd
+++ Scenes/Base/BaseScene.gd
@@ -6,10 +6,11 @@ export (Resource) var elo setget set_rank
 
 
 func set_rank(e):
-	elo = e
-	var badge = $Structure/VBoxContainer/Logo/VBoxContainer/MarginContainer/Badge
-	var title = $Structure/VBoxContainer/Logo/VBoxContainer/Title
-	var bg = $Structure/BG
-	badge.texture = e.badge
-	title.texture = e.title
-	bg.texture = e.bg
+	if Engine.editor_hint:
+		elo = e
+		var badge = $Structure/VBoxContainer/Logo/VBoxContainer/MarginContainer/Badge
+		var title = $Structure/VBoxContainer/Logo/VBoxContainer/Title
+		var bg = $Structure/BG
+		badge.texture = e.badge
+		title.texture = e.title
+		bg.texture = e.bg
